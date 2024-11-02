add79b90250061b9a7563479b27cfc522c2a5667
Martin <martingramajo08@gmail.com>
2024-11-02 15:40:27 -0300

    creamos el endpoint para listar todos y agregamos para hacer una pagination simple

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const take = +(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: "take tiene que ser un numero" },
+      { status: 400 }
+    );
+  }
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { message: "skip tiene que ser un numero" },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
+
+  return NextResponse.json(todos);
+}
