49dd03dad07a0195759c93a93618ac6b1fd8348f
Martin <martingramajo08@gmail.com>
2024-11-02 15:34:28 -0300

    creada la seed y agregado los primeros valores

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,6 +1,22 @@
+import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
+
+
+    await prisma.todo.deleteMany();
+
+  const todo = await prisma.todo.createMany({
+    data: [
+      { title: "Sagrada", complete: true },
+      { title: "Cartografos" },
+      { title: "Agricola" },
+      { title: "Catan" },
+      { title: "Azul" },
+    ],
+  });
+  console.log("🚀 ~ GET ~ todo:", todo);
+
   return NextResponse.json({
     message: "Seed Executed",
   });
